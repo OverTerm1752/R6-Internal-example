3df54965d548f256d821d244ec1911155987e189
GAMEBOX <MY@ass.com>
2024-08-18 20:25:36 +0200

    CTRL + V

--- Bypass.h
+++ Bypass.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "get.h"
+#include "Driver.h"
 
 void bypass()
 {
 
+	// Ubisoft's anti-cheat bypass (credits to OVERHEAD)
 
- uint64_t bypassbase = get::client + 0x8471660;
+uint64_t bypassbase = get::client + 0x8A1A4B0;
 float* bypass = reinterpret_cast<float*>(bypassbase + 9);
 *bypass = 9;
 
 
 }
+
